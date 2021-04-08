981b06c5009b8d09d779c4ac44fc374bf70913d5
Benjamin Howe <ben@bh96.uk>
2021-04-08 15:12:24 +0100

    Added modulo operator

--- test_cpu.py
+++ test_cpu.py
@@ -10,5 +10,8 @@ class TestCPU(unittest.TestCase):
         self.assertEqual(cpu.pc, 4478)
         self.assertEqual(cpu.getStatement(cpu.pc), 'goto 7760')
 
+    def test_mod(self):
+        self.assertEqual(cpu.calc('% 5 2'), 1)
+
 if __name__ == '__main__':
     unittest.main()
